64acafb523d8e7e5c7ba6e64fdb65f2d36ec1545
Benjamin Wolff <wolff@zbmed.de>
2021-04-15 11:12:59 +0200

    github actions test

--- systems.py
+++ systems.py
@@ -28,7 +28,6 @@ class Recommender(object):
         self.idx = None
 
     def index(self):
-        # start indexing
         self.idx = []
         with jsonlines.open('./data/gesis-search/datasets/dataset.jsonl') as reader:
             for obj in reader:
